3ddc921bc31a2a88c9b74974aaa4103a84ef09c0
ericg <ericgorr@gmail.com>
2022-03-05 14:57:27 -0500

    some progressing on passing data

--- CDPassingQ/NameViewer.swift
+++ CDPassingQ/NameViewer.swift
@@ -12,75 +12,132 @@
 //
 
 import SwiftUI
+import CoreData
 
-
-
-struct NameViewer: View
+enum TrustReason: String, Identifiable, CaseIterable
 {
-  @Environment ( \.presentationMode) var presentationMode
-  @Environment( \.managedObjectContext ) private var moc
-  
-  @State private var name:    String      = ""
-  
-  var body: some View
-  {
-    Form
+    var id: UUID
     {
-      Section( header: Text( "Information" ) )
-      {
-        TextField( "Name", text: $name )
-      }
-    }
-    .toolbar
-    {
-      Button()
-      {
-        if ( saveName() )
-        {
-          self.presentationMode.wrappedValue.dismiss()
-        }
-      }
-    label:
-      {
-        Text( "Save" )
-      }
-    }
-  }
-  
-  
-  
-  private func saveName() -> Bool
-  {
-    let item = Item( context: moc )
-    
-    do
-    {
-      print( self.name )
-      
-      item.name = self.name
-
-      try moc.save()
-      
-      return true
-    }
-    catch
-    {
-      print( error )
-      print( error.localizedDescription )
+        return UUID();
     }
     
-    self.moc.rollback();
-    
-    return false
-  }
+    case unknown         = "Unknown";
+    case legalOnly       = "Legal Only";
+    case goodLabeling    = "Good Labeling";
+    case facilityClean   = "Facility Clean";
+    case detailedAnswers = "Detailed Answers";
+    case unresponsive    = "Unresponsive";
 }
 
 
 
-struct NameViewer_Previews: PreviewProvider
+extension TrustReason
 {
-  static var previews: some View
-  {
-    NameViewer()
-  }
+    var title: String
+    {
+        switch self
+        {
+            case .unknown:
+                return "Unknown";
+                
+            case .legalOnly:
+                return "Legal Only";
+                
+            case .goodLabeling:
+                return "Good Labeling";
+                
+            case .facilityClean:
+                return "Facility Clean";
+                
+            case .detailedAnswers:
+                return "Detailed Answers";
+                
+            case .unresponsive:
+                return "Unresponsive";
+        }
+    }
+}
+
+
+
+struct NameViewer: View {
+    @Environment( \.presentationMode )     var         presentationMode
+    @Environment( \.managedObjectContext ) private var moc
+    
+    @State private var name: String = ""
+    @State private var reason: TrustReason = .unknown
+
+    var itemID: NSManagedObjectID?
+    
+    var body: some View {
+        Form {
+            Section( header: Text( "Information" ) ) {
+                TextField( "Name", text: $name )
+            }
+            
+            Section( header: Text( "Trust" ) ) {
+                Picker( "Reason", selection: $reason ) {
+                    ForEach( TrustReason.allCases ) { trustReason in
+                        Text( trustReason.title ).tag( trustReason )
+                    }
+                }
+            }
+        }
+        .toolbar {
+            Button() {
+                if ( saveName() ) {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            } label: {
+                Text( "Save" )
+            }
+        }
+        .onAppear {
+            print( "on appear" )
+            
+            guard let theID = itemID,
+                  let item = moc.object( with: theID ) as? Item else {
+                      return
+                  }
+            
+            print( "passed guard" )
+            
+            if let itemName = item.name {
+                name = itemName
+            }
+            
+            print( name )
+        }
+    }
+    
+    
+    
+    private func saveName() -> Bool {
+        let item = Item( context: moc )
+        
+        do {
+            print( self.name )
+            
+            item.name = self.name
+            
+            try moc.save()
+            
+            return true
+        } catch {
+            print( error )
+            print( error.localizedDescription )
+        }
+        
+        self.moc.rollback();
+        
+        return false
+    }
+}
+
+
+
+struct NameViewer_Previews: PreviewProvider {
+    static var previews: some View {
+        NameViewer()
+    }
 }
