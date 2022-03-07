bebeb51d7d072b9f9fb22690527c4fc970125ec2
ericg <ericgorr@gmail.com>
2022-03-06 19:37:52 -0500

    solution does not work yet

--- CDPassingQ/ContentView.swift
+++ CDPassingQ/ContentView.swift
@@ -9,65 +9,65 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment( \.managedObjectContext ) private var viewContext
-    
-    @FetchRequest( sortDescriptors: [ NSSortDescriptor( keyPath: \Item.name, ascending: true ) ],
-                   animation:       .default )
-    private var items: FetchedResults<Item>
-    
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach( items ) { item in
-                    NavigationLink {
-                        NameViewer( item: item )
-                    } label: {
-                        Text( item.name! )
-                    }
-                }
-                .onDelete( perform: deleteItems )
-            }
-            .toolbar {
-                ToolbarItem( placement: .navigationBarTrailing ) {
-                    EditButton()
-                }
-                
-                ToolbarItem {
-                    Button() {
-                        print( "Add Item" )
-                    } label: {
-                        NavigationLink {
-                            NameViewer();
-                        } label: {
-                            Label( "Add Item", systemImage: "plus" )
-                        }
-                    }
-                }
-            }
+  @Environment( \.managedObjectContext ) private var moc
+  
+  @FetchRequest( sortDescriptors: [ NSSortDescriptor( keyPath: \Item.name, ascending: true ) ],
+                 animation:       .default )
+  private var items: FetchedResults<Item>
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach( items ) { item in
+          NavigationLink {
+            NameViewer( item: item )
+          } label: {
+            Text( item.name! )
+          }
         }
-    }
-    
-    
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        .onDelete( perform: deleteItems )
+      }
+      .toolbar {
+        ToolbarItem( placement: .navigationBarTrailing ) {
+          EditButton()
         }
+        
+        ToolbarItem {
+          Button() {
+            print( "Add Item" )
+          } label: {
+            NavigationLink {
+              NameViewer( item: Item( context: moc ) );
+            } label: {
+              Label( "Add Item", systemImage: "plus" )
+            }
+          }
+        }
+      }
     }
+  }
+  
+  
+  
+  private func deleteItems(offsets: IndexSet) {
+    withAnimation {
+      offsets.map { items[$0] }.forEach(moc.delete)
+      
+      do {
+        try moc.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+      }
+    }
+  }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+  static var previews: some View {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+  }
 }
--- CDPassingQ/NameViewer.swift
+++ CDPassingQ/NameViewer.swift
@@ -16,134 +16,109 @@ import CoreData
 
 enum TrustReason: String, Identifiable, CaseIterable
 {
-    var id: UUID
-    {
-        return UUID();
-    }
-    
-    case unknown         = "Unknown";
-    case legalOnly       = "Legal Only";
-    case goodLabeling    = "Good Labeling";
-    case facilityClean   = "Facility Clean";
-    case detailedAnswers = "Detailed Answers";
-    case unresponsive    = "Unresponsive";
+  var id: UUID
+  {
+    return UUID();
+  }
+  
+  case unknown         = "Unknown";
+  case legalOnly       = "Legal Only";
+  case goodLabeling    = "Good Labeling";
+  case facilityClean   = "Facility Clean";
+  case detailedAnswers = "Detailed Answers";
+  case unresponsive    = "Unresponsive";
 }
 
 
 
 extension TrustReason
 {
-    var title: String
+  var title: String
+  {
+    switch self
     {
-        switch self
-        {
-            case .unknown:
-                return "Unknown";
-                
-            case .legalOnly:
-                return "Legal Only";
-                
-            case .goodLabeling:
-                return "Good Labeling";
-                
-            case .facilityClean:
-                return "Facility Clean";
-                
-            case .detailedAnswers:
-                return "Detailed Answers";
-                
-            case .unresponsive:
-                return "Unresponsive";
-        }
+      case .unknown:
+        return "Unknown";
+        
+      case .legalOnly:
+        return "Legal Only";
+        
+      case .goodLabeling:
+        return "Good Labeling";
+        
+      case .facilityClean:
+        return "Facility Clean";
+        
+      case .detailedAnswers:
+        return "Detailed Answers";
+        
+      case .unresponsive:
+        return "Unresponsive";
     }
+  }
 }
 
 
 
 struct NameViewer: View {
-    @Environment( \.presentationMode )     var         presentationMode
-    @Environment( \.managedObjectContext ) private var moc
-    
-    @State private var name: String = ""
-    @State private var reason: TrustReason = .unknown
-
-//    var itemID: NSManagedObjectID?
-    
-    var item: Item?
-    
-    var body: some View {
-        Form {
-            Section( header: Text( "Information" ) ) {
-                TextField( "Name", text: $name )
-            }
-            
-            Section( header: Text( "Trust" ) ) {
-                Picker( "Reason", selection: $reason ) {
-                    ForEach( TrustReason.allCases ) { trustReason in
-                        Text( trustReason.title ).tag( trustReason )
-                    }
-                }
-            }
-        }
-        .toolbar {
-            Button() {
-                if ( saveName() ) {
-                    self.presentationMode.wrappedValue.dismiss()
-                }
-            } label: {
-                Text( "Save" )
-            }
-        }
-        .onAppear {
-            print( "on appear" )
+  @Environment( \.presentationMode )     var         presentationMode
+  @Environment( \.managedObjectContext ) private var moc
   
-//            guard let theID = itemID,
-//                  let item = moc.object( with: theID ) as? Item else {
-//                      return
-//                  }
-
-            guard let theItem = item else {
-                      return
-                  }
-            
-            print( "passed guard" )
-
-            if let itemName = theItem.name {
-                name = itemName
-            }
-            
-            print( name )
+  @State private var reason:  TrustReason = .unknown
+  
+  @ObservedObject var item: Item
+  
+  var body: some View {
+    Form {
+      Section( header: Text( "Information" ) ) {
+        if let name = Binding($item.name) {
+          TextField("Name", text: name)
         }
+      }
+      
+      Section( header: Text( "Trust" ) ) {
+        Picker( "Reason", selection: $reason ) {
+          ForEach( TrustReason.allCases ) { trustReason in
+            Text( trustReason.title ).tag( trustReason )
+          }
+        }
+      }
+    }
+    .toolbar {
+      Button() {
+        if ( saveName() ) {
+          self.presentationMode.wrappedValue.dismiss()
+        }
+      } label: {
+        Text( "Save" )
+      }
+    }
+  }
+  
+  
+  
+  private func saveName() -> Bool {
+    do {
+      print( item.name ?? "<unknown name!!>" )
+          
+      try moc.save()
+      
+      return true
+    } catch {
+      print( error )
+      print( error.localizedDescription )
     }
     
+    self.moc.rollback();
     
-    
-    private func saveName() -> Bool {
-        let item = Item( context: moc )
-        
-        do {
-            print( self.name )
-            
-            item.name = self.name
-            
-            try moc.save()
-            
-            return true
-        } catch {
-            print( error )
-            print( error.localizedDescription )
-        }
-        
-        self.moc.rollback();
-        
-        return false
-    }
+    return false
+  }
 }
 
 
 
-struct NameViewer_Previews: PreviewProvider {
-    static var previews: some View {
-        NameViewer()
-    }
-}
+//struct NameViewer_Previews: PreviewProvider {
+//  static var previews: some View {
+//    NameViewer()
+//  }
+//}
